eec6a81b4aca7478c9869b5e0f8a0c49015fffc0
jyr <jingyirui123@gmail.com>
2025-05-31 10:17:38 +0800

    实现窗口

--- dzy/text_deitor/src/main.rs
+++ dzy/text_deitor/src/main.rs
@@ -1,4 +1,36 @@
-use iced::{Sandbox};
-fn main() {
-    
+use iced::{Sandbox,Settings};
+use iced::widget::{text};
+fn main() -> iced::Result{
+    Editor::run(Settings::default())
+}
+
+struct Editor{
+
+}
+#[derive(Debug)]
+enum Message{
+
+}
+
+impl Sandbox for Editor{
+    type Message=Message;
+
+    fn new()->Self{
+        Self{
+
+        }
+    }
+
+    fn title(&self)->String{
+        String::from("Text Editor")
+    }
+    fn update(&mut self, message:Message){
+        match message{
+        }
+    }
+
+    fn view(&self)->iced::Element<'_,Message>{
+        text("This is the text editor").into()
+    }
+
 }
