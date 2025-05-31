508adca3c869229e9ae05259312595835d12078a
jyr <jingyirui123@gmail.com>
2025-05-31 13:24:52 +0800

    实现基础框架

--- dzy/text_deitor/src/main.rs
+++ dzy/text_deitor/src/main.rs
@@ -1,15 +1,17 @@
-use iced::{Sandbox,Settings};
-use iced::widget::{text};
+use iced::Theme;
+use iced::{Sandbox,Settings,Length};
+use iced::widget::{container, horizontal_space, row, text, text_editor};
+use iced::widget::column;
 fn main() -> iced::Result{
     Editor::run(Settings::default())
 }
 
 struct Editor{
-
+    content: text_editor::Content,
 }
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 enum Message{
-
+    Edit(text_editor::Action)
 }
 
 impl Sandbox for Editor{
@@ -17,7 +19,7 @@ impl Sandbox for Editor{
 
     fn new()->Self{
         Self{
-
+            content: text_editor::Content::new(),
         }
     }
 
@@ -26,11 +28,40 @@ impl Sandbox for Editor{
     }
     fn update(&mut self, message:Message){
         match message{
+            Message::Edit(action)=>self.content.perform(action)
         }
     }
 
-    fn view(&self)->iced::Element<'_,Message>{
-        text("This is the text editor").into()
-    }
-
+        fn view(&self) -> iced::Element<'_, Message> {
+        let input_content = text_editor(&self.content).on_action(Message::Edit).height(Length::Fill).style(self.theme()).into();
+        let poisition = {
+        let (line, column) = &self.content.cursor_position();
+        text(format!("{}:{}", line + 1, column + 1))
+    };
+    let status_bar = row!(horizontal_space(), poisition);
+    container(column!(input_content, status_bar)).padding(10).into()
 }
+
+   
+        
+
+    fn theme(&self)-> iced::Theme{
+        Theme::Dark
+    }
+    
+    fn style(&self) -> iced::theme::Application {
+        iced::theme::Application::default()
+    }
+    
+    fn scale_factor(&self) -> f64 {
+        1.0
+    }
+    
+    fn run(settings: Settings<()>) -> Result<(), iced::Error>
+    where
+        Self: 'static + Sized,
+    {
+        <Self as iced::Application>::run(settings)
+    }
+}
+
