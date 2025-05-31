756ab96886864202d00fe3624c9f50d03203fff5
jyr <jingyirui123@gmail.com>
2025-05-31 15:19:52 +0800

    实现路径追踪

--- dzy/text_deitor/src/main.rs
+++ dzy/text_deitor/src/main.rs
@@ -11,11 +11,12 @@ fn main() -> iced::Result{
 struct Editor{
     content: text_editor::Content,
     error: Option<Error>,
+    path: Option<PathBuf>,
 }
 #[derive(Debug,Clone)]
 enum Message{
     Edit(text_editor::Action),
-    FileOpened(Result<Arc<String>,Error>),
+    FileOpened(Result<(PathBuf,Arc<String>),Error>),
     Open,
     New,
 }
@@ -31,6 +32,7 @@ impl Application for Editor{
             Self{
             content: text_editor::Content::with_text(include_str!("./main.rs")),
             error: None,
+            path: None,
         },
         Command::perform(load_file(default_load_file()),Message::FileOpened)
         )
@@ -45,8 +47,9 @@ impl Application for Editor{
                 self.content.perform(action);
                 Command::none()
             }
-            Message::FileOpened(Ok(contents))=>{
+            Message::FileOpened(Ok((path,contents)))=>{
                 self.content = text_editor::Content::with_text(&contents);
+                self.path = Some(path);
                 Command::none()
             }
             Message::FileOpened(Err(error))=>{
@@ -58,6 +61,7 @@ impl Application for Editor{
             }
             Message::New=>{
                 self.content = text_editor::Content::new();
+                self.path = None;
                 Command::none()
             }
             
@@ -78,7 +82,16 @@ impl Application for Editor{
         let (line, column) = &self.content.cursor_position();
         text(format!("{}:{}", line + 1, column + 1))
         };
-    let status_bar = row!(horizontal_space(), poisition);
+        let file_path=if let Some(Error::IOFailed(error))=self.error.as_ref(){
+            text(error.to_string())
+        }else{
+            match self.path.as_deref().and_then(Path::to_str){
+                Some(path)=>text(path).size(15),
+                None=>text("New File").size(15),
+
+            }
+        };
+    let status_bar = row!(file_path,horizontal_space(), poisition);
     container(column!(controls,input_content, status_bar)).padding(10).into()
 }
 
@@ -105,20 +118,21 @@ enum Error {
     DialogClosed,
 }
 
-async fn load_file(path:impl AsRef<Path>) -> Result<Arc<String>,Error> {
-    tokio::fs::read_to_string(path).await.map(Arc::new).map_err(|error|Error::IOFailed(error.kind()))
+async fn load_file(path:impl AsRef<Path>) -> Result<(PathBuf,Arc<String>),Error> {
+    let contents =tokio::fs::read_to_string(path.as_ref()).await.map(Arc::new).map_err(|error|Error::IOFailed(error.kind()))?;
+    Ok((path.as_ref().to_path_buf(),contents))
 }
 
 fn default_load_file() -> PathBuf {
     PathBuf::from(format!("{}/rsc/main.rs",env!("CARGO_MANIFEST_DIR")))
 
 }
-async fn pick_flie()->Result<Arc<String>,Error> {
+async fn pick_flie()->Result<(PathBuf,Arc<String>),Error> {
    let file_path= rfd::AsyncFileDialog::new().set_title("Choose a file")
-                                .pick_file()
-                                .await
-                                .ok_or(Error::DialogClosed)
-                                .map(|fileHandle|fileHandle.path().to_owned())?;
+                .pick_file()
+                .await
+                .ok_or(Error::DialogClosed)
+                .map(|filehandle|filehandle.path().to_owned())?;
     load_file(file_path).await
 }
 
