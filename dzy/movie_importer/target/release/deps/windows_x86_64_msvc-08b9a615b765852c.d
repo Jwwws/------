86ddfa8830c103be7b0a2d0588d9e06195f453fd
jyr <jingyirui123@gmail.com>
2025-05-31 14:26:43 +0800

    实现文件打开以及错误部分处理

---
+++ dzy/movie_importer/target/release/deps/windows_x86_64_msvc-08b9a615b765852c.d
@@ -0,0 +1,7 @@
+D:\大学\大二\可信开源上机\dzy\movie_importer\target\release\deps\libwindows_x86_64_msvc-08b9a615b765852c.rmeta: C:\Users\Jw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.52.6\src\lib.rs
+
+D:\大学\大二\可信开源上机\dzy\movie_importer\target\release\deps\libwindows_x86_64_msvc-08b9a615b765852c.rlib: C:\Users\Jw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.52.6\src\lib.rs
+
+D:\大学\大二\可信开源上机\dzy\movie_importer\target\release\deps\windows_x86_64_msvc-08b9a615b765852c.d: C:\Users\Jw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.52.6\src\lib.rs
+
+C:\Users\Jw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.52.6\src\lib.rs:
--- dzy/text_deitor/src/main.rs
+++ dzy/text_deitor/src/main.rs
@@ -1,10 +1,9 @@
-use iced::Theme;
-use std::env;
+
 use std::sync::Arc;
 use std::path::{Path,PathBuf};
 use std::io::ErrorKind;
-use iced::{Command,executor,Application,Settings,Length};
-use iced::widget::{column,container, horizontal_space, row, text, text_editor};
+use iced::{Theme,Command,executor,Application,Settings,Length};
+use iced::widget::{button,column,container, horizontal_space, row, text, text_editor};
 fn main() -> iced::Result{
     Editor::run(Settings::default())
 }
@@ -17,6 +16,7 @@ struct Editor{
 enum Message{
     Edit(text_editor::Action),
     FileOpened(Result<Arc<String>,Error>),
+    Open
 }
 
 impl Application for Editor{
@@ -52,18 +52,27 @@ impl Application for Editor{
                 self.error = Some(error);
                 Command::none()
             }
+            Message::Open=>{
+                Command::perform(pick_flie(),Message::FileOpened)
+            }
         }
 
     }
 
-        fn view(&self) -> iced::Element<'_, Message> {
-        let input_content = text_editor(&self.content).on_action(Message::Edit).height(Length::Fill).into();
+    fn view(&self) -> iced::Element<'_, Message> {
+        let controls ={
+            button("Open").on_press(Message::Open)
+        };
+        let input_content = text_editor(&self.content)
+            .on_action(Message::Edit)
+            .height(Length::Fill)
+            .into();
         let poisition = {
         let (line, column) = &self.content.cursor_position();
         text(format!("{}:{}", line + 1, column + 1))
-    };
+        };
     let status_bar = row!(horizontal_space(), poisition);
-    container(column!(input_content, status_bar)).padding(10).into()
+    container(column!(controls,input_content, status_bar)).padding(10).into()
 }
 
    
@@ -92,7 +101,7 @@ impl Application for Editor{
 #[derive(Debug,Clone)]
 enum Error {
     IOFailed(ErrorKind),
-    
+    DialogClosed,
 }
 
 async fn load_file(path:impl AsRef<Path>) -> Result<Arc<String>,Error> {
@@ -103,6 +112,13 @@ fn default_load_file() -> PathBuf {
     PathBuf::from(format!("{}/rsc/main.rs",env!("CARGO_MANIFEST_DIR")))
 
 }
-
+async fn pick_flie()->Result<Arc<String>,Error> {
+   let file_path= rfd::AsyncFileDialog::new().set_title("Choose a file")
+                                .pick_file()
+                                .await
+                                .ok_or(Error::DialogClosed)
+                                .map(|fileHandle|fileHandle.path().to_owned())?;
+    load_file(file_path).await
+}
 
 
